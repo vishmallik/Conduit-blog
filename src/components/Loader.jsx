1718fb6220ff3d98dce5926269e453efd3082f03
visawjeet mallik <vishmallik@gmail.com>
2022-11-30 09:17:39 +0530

    add loader style

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 import "../stylesheets/_loader.css";
 export default function Loader() {
   return (
-    <div className="lds-ripple mx-auto mt-10 block py-16 text-center">
+    <div className="lds-ripple mx-auto mt-10 block min-h-screen py-16 text-center">
       <div></div>
       <div></div>
     </div>
